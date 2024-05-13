18052abb32a755e949f27fb96ee9b3830f6dfce4
Aibekin <77619101+Aibekin@users.noreply.github.com>
2024-05-14 00:23:52 +0500

    return date

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,7 +40,7 @@ export const Home = () => {
 								title={obj.title}
 								imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}` : ''}
 								user={obj.user}
-								createdAt={new Intl.DateTimeFormat('ru_RU').format(obj.createdAt)}
+								createdAt={obj.createdAt}
 								viewsCount={obj.viewsCount}
 								commentsCount={3}
 								tags={obj.tags}
